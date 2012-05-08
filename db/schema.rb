6d7f94c3df5a094f8b9ac7477ba1705166c66c15
Mike Voytovich <mikev@duffresearch.com>
2012-05-07 22:18:03 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508043802) do
+ActiveRecord::Schema.define(:version => 20120508045659) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(:version => 20120508043802) do
     t.integer  "user_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "project_id"
   end
 
+  add_index "tasks", ["project_id"], :name => "index_tasks_on_project_id"
   add_index "tasks", ["user_id"], :name => "index_tasks_on_user_id"
 
   create_table "users", :force => true do |t|
