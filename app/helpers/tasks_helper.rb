b538df773dc0962127a2d13da6a48139091d95e7
Mike Voytovich <mikev@duffresearch.com>
2012-05-07 21:18:31 -0700

    build project list from rails; make project name a select dropdown

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+
+def projectSelect()
+ out = "<select>"
+ names = ["Nest", "Vacation", "Sick Time"]
+ names.each do |name|
+   out += "<option value=\"#{name}\">#{name}</option>"
+ end
+ out += "</select>"
+  render :inline => out
+end
+
 end
