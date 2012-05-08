e37f339de14b30c98509c854b0fffcf4cb31381e
Mike Voytovich <mikev@duffresearch.com>
2012-05-08 09:15:14 -0700

    show tasks on project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
+    @tasks = @project.tasks
 
     respond_to do |format|
       format.html # show.html.erb
